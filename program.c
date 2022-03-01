037d7508be374799a93a816ef03e4bbecb83848b
roshini22 <100598649+roshini22@users.noreply.github.com>
2022-03-01 12:42:53 +0530

    Update program.c

--- program.c
+++ program.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 int main()
 {
-printf("Hello world");
+  int n;
+printf("Entera number:");
+scanf("%d",&n)
+if(n%2==0)
+{
+  printf("The num is even");
+}else
+{
+  printf("The num is odd")' 
+}
 return 0;
 }

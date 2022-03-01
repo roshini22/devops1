b1d1e64aca7797f42b3191ab730aff8b82b7dbe0
Arul Praveena Boopalagan <96655829+arulveena@users.noreply.github.com>
2022-03-01 12:48:01 +0530

    Update program.c
    some changes added

--- program.c
+++ program.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
-int main()
+void main()
 {
   int n;
 printf("Entera number:");
-scanf("%d",&n)
+scanf("%d",&n);
 if(n%2==0)
-{
   printf("The num is even");
-}else
-{
-  printf("The num is odd")' 
-}
-return 0;
+else
+  printf("The num is odd");
 }
